4fdd45aee34bd9d4c8a5af63313a4fecf7163899
Nils Treichel <uqqol@student.kit.edu>
2024-01-02 14:03:05 +0100

    Dateien kopiert

---
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -0,0 +1,49 @@
+function updateLocation() {
+    // MapManager mit API Key initialisieren
+    const mapManager = new MapManager('c8nLw5oe3DkrjZlfHY3KTVguYfnvcUHw');
+
+    // Lesen Sie die geeigneten Formularfelder im DOM aus
+    const taggingLatitudeInput = document.getElementById('latitude');
+    const taggingLongitudeInput = document.getElementById('longitude');
+    const discoveryLatitudeInput = document.getElementById('disc_latitude');
+    const discoveryLongitudeInput = document.getElementById('disc_longitude');
+
+    const mapImage = document.getElementById('mapView');
+
+    var latitude = taggingLatitudeInput.value;
+    var longitude = taggingLongitudeInput.value;
+
+    if(latitude === "" && longitude === "") {
+        LocationHelper.findLocation((location) => {
+            latitude = location.latitude;
+            longitude = location.longitude;
+
+            // Werte in Tagging und Discovery updaten
+            taggingLatitudeInput.value = latitude;
+            taggingLongitudeInput.value = longitude;
+            discoveryLatitudeInput.value = latitude;
+            discoveryLongitudeInput.value = longitude;
+
+            const mapUrl = mapManager.getMapUrl(latitude, longitude, JSON.parse(document.getElementById("mapView").getAttribute("data-tags")));
+
+            // Map URL als Bild einfügen
+            mapImage.src = mapUrl;
+        });
+    } else {
+                // Werte in Tagging und Discovery updaten
+                taggingLatitudeInput.value = latitude;
+                taggingLongitudeInput.value = longitude;
+                discoveryLatitudeInput.value = latitude;
+                discoveryLongitudeInput.value = longitude;
+        
+                // Map URL mit Mapmanager abfragen
+                const mapUrl = mapManager.getMapUrl(latitude, longitude, JSON.parse(document.getElementById("mapView").getAttribute("data-tags")));
+        
+                // Map URL als Bild einfügen
+                mapImage.src = mapUrl;
+
+    }
+}
+
+// Wait for the page to fully load its DOM content, then call updateLocation
+document.addEventListener("DOMContentLoaded", updateLocation);
