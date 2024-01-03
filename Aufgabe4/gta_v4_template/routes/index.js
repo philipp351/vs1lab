0ac4874235b4fca529dd5ea56c5589378f9f2967
Nils Treichel <uqqol@student.kit.edu>
2024-01-03 15:19:06 +0100

    Vorbereitung durch

--- Aufgabe4/gta_v4_template/routes/index.js
+++ Aufgabe4/gta_v4_template/routes/index.js
@@ -27,7 +27,68 @@ const GeoTag = require('../models/geotag');
 // eslint-disable-next-line no-unused-vars
 const GeoTagStore = require('../models/geotag-store');
 
-// App routes (A3)
+const geoTagStore = new GeoTagStore();
+
+router.get('/', (req, res) => {
+  res.render('index', {taglist: geoTagStore.geotags, latitude: "", longitude: "" })
+});
+
+/**
+ * Route '/tagging' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the tagging form in the body.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Based on the form data, a new geotag is created and stored.
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the new geotag.
+ * To this end, "GeoTagStore" provides a method to search geotags 
+ * by radius around a given location.
+ */
+
+router.post('/tagging', (req, res) => {
+  const name = req.body.name;
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
+  const hashtag = req.body.hashtag;
+
+  geoTagStore.addGeoTag(name, latitude, longitude, hashtag);
+  res.render('index', {
+    taglist: geoTagStore.getNearbyGeoTags(latitude, longitude, 100),
+    latitude: latitude,
+    longitude: longitude
+  });
+});
+
+
+/**
+ * Route '/discovery' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the discovery form in the body.
+ * This includes coordinates and an optional search term.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the given coordinates.
+ * If a search term is given, the results are further filtered to contain 
+ * the term as a part of their names or hashtags. 
+ * To this end, "GeoTagStore" provides methods to search geotags 
+ * by radius and keyword.
+ */
+
+router.post('/discovery', (req, res) => {
+  const latitude = req.body.disc_latitude;
+  const longitude = req.body.disc_longitude;
+  const searchTerm = req.body.searchterm;
+  res.render('index', {
+    taglist: geoTagStore.searchNearbyGeoTags(latitude, longitude, 100, searchTerm),
+    latitude: latitude,
+    longitude: longitude
+  });
+});
 
 /**
  * Route '/' for HTTP 'GET' requests.
