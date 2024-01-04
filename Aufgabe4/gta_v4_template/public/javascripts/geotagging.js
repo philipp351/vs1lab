1407adbd19b85f9738c8d3060187c911b553a8ba
Nils Treichel <nilstreichel@web.de>
2024-01-04 10:35:44 +0100

    aktueller Stand

--- Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -1,3 +1,79 @@
+
+
+const taggingButton = document.getElementById("taggingButton");
+const discoveryButton = document.getElementById("discoveryButton");
+
+taggingButton.addEventListener("click", handleTaggingButtonClick);
+discoveryButton.addEventListener("click", handleDiscoveryButtonClick);
+
+async function handleTaggingButtonClick() {
+    const form = document.getElementById('tag-form');
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+    const name = document.getElementById("name").value;
+    const latitude = document.getElementById("latitude").value;
+    const longitude = document.getElementById("longitude").value;
+    const hashtag = document.getElementById("tagTextField").value;
+
+    const response = await fetch(`/api/geotags`, {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            latitude: latitude,
+            longitude: longitude,
+            hashtag: hashtag
+        })
+    });
+    const data = await response.json();
+    console.log(response);
+    let geotaglist;
+
+    fetch('/api/geotags')
+        .then(response => response.json())
+        .then(data => {
+            geotaglist = data;
+            refreshElements(geotaglist);
+        });
+}
+
+async function handleDiscoveryButtonClick() {
+    const form = document.getElementById('discoveryFilterForm');
+    if (!form.checkValidity()) {
+        form.reportValidity();
+    }
+    const name = document.getElementById("searchterm").value;
+    const latitude = document.getElementById("latitude").value;
+    const longitude = document.getElementById("longitude").value;
+
+    const url = `/api/geotags?name=${name}&latitude=${latitude}&longitude=${longitude}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            refreshElements(data);
+            console.log(data);
+        });
+}
+
+function refreshElements(geotaglist) {
+    const ul = document.getElementById('discoveryResults');
+    ul.innerHTML = "";
+    for (const tag of geotaglist) {
+        const li = document.createElement("li");
+        li.textContent = `${tag.name} (${tag.latitude},${tag.longitude}) ${tag.hashtag}`;
+        ul.appendChild(li);
+    }
+    const mapviewdata = document.getElementById("mapView");
+    mapviewdata.setAttribute("data-tags", JSON.stringify(geotaglist));
+    updateLocation();
+}
+
+
 function updateLocation() {
     // MapManager mit API Key initialisieren
     const mapManager = new MapManager('c8nLw5oe3DkrjZlfHY3KTVguYfnvcUHw');
